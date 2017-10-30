1c901d5f2da9ba4cd841ed2e9428a0579a46faa1
Arthur Gorr <art.gorr@gmail.com>
2017-10-29 21:40:15 -0500

    Introducing Nancy and default route

---
+++ BillPayApi.Core/BillPayApi.Core/Home/HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillPayApi.Core.Home
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => "Super Duper Happy Path running on .NET Core");
+        }
+    }
+}
